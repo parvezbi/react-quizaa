6ba1e376433f32e7866af406f5876c3de5c0514e
Md Parvez Habib <parvezmd082@gmail.com>
2023-09-11 18:01:47 +0600

    11-09-Office-class11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
 import Layout from "./Layout";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
@@ -11,15 +13,19 @@ export default function App() {
   return (
     <Router>
       <AuthProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </Layout>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route element={<PublicRoute />}>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="/*" element={<PrivateRoute />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
+          </Routes>
+        </Layout>
       </AuthProvider>
     </Router>
   );
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,17 +1,14 @@
-import { Link } from "react-router-dom";
 import thumbnail from "../assets/images/3.jpg";
 import classes from "./styles/Video.module.css";
 export default function Video() {
   return (
-    <Link to="/quiz">
-      <div className={classes.video}>
-        <img src={thumbnail} alt="" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-        <div className={classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
-        </div>
+    <div className={classes.video}>
+      <img src={thumbnail} alt="" />
+      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <div className={classes.qmeta}>
+        <p>10 Questions</p>
+        <p>Score : Not taken yet</p>
       </div>
-    </Link>
+    </div>
   );
 }
